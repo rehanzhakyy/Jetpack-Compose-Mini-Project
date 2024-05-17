aa55a66d32a47a5397b52e30d968ff282a943d4d
rehanzhakyy <hannn9491@gmail.com>
2024-05-17 10:36:45 +0700

    create the content composable function

--- BizCard-App/app/src/main/java/com/example/bizcard_app/MainActivity.kt
+++ BizCard-App/app/src/main/java/com/example/bizcard_app/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -87,14 +88,44 @@ fun CreateBizCard() {
                         Log.d("Clicked", "CreateBizCard: Button Clicked")
                     }
                 ) {
-                    Text("Portfolio",
-                    style = MaterialTheme.typography.bodyMedium)
+                    Text(
+                        "Portfolio",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
                 }
             }
 
         }
     }
 }
+
+@Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2 .dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    Text(text = "Project go here!!")
+}
+
 
 @Composable
 private fun CreateInfo() {
