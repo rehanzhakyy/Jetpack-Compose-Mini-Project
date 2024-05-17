847061c45feb43186ab847fc306443c5b8e438ec
rehanzhakyy <hannn9491@gmail.com>
2024-05-17 10:06:58 +0700

    adding a Column and a Divider

--- BizCard-App/app/src/main/java/com/example/bizcard_app/MainActivity.kt
+++ BizCard-App/app/src/main/java/com/example/bizcard_app/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,11 +22,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -68,20 +72,36 @@ fun CreateBizCard() {
             colors = CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
-            Surface(
-                modifier = Modifier
-                    .size(150.dp)
-                    .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.LightGray),
-                shadowElevation = 4.dp,
-                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(painter = painterResource(id = R.drawable.profilezaki), contentDescription = "Profile Image",
-                    modifier = Modifier.size(135.dp),
-                    contentScale = ContentScale.Crop)
+                CreateImageProfile()
+                Divider()
             }
+
         }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profilezaki),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
