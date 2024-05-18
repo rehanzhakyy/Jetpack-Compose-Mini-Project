217696f4ede5a22de64a7cdc465bd38f38ab521b
rehanzhakyy <hannn9491@gmail.com>
2024-05-18 19:43:26 +0700

    finish up top header function

--- JetTip-App/app/src/main/java/com/example/jettip_app/MainActivity.kt
+++ JetTip-App/app/src/main/java/com/example/jettip_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,9 +17,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettip_app.ui.theme.JetTipAppTheme
@@ -28,9 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApp {
-                Text(text = "Hello world")
-            }
+            TopHeader()
         }
     }
 }
@@ -48,17 +50,28 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 @Preview
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             .clip(
                 shape = RoundedCornerShape(corner = CornerSize(12.dp))
-            )
+            ), color = Color(0xFFE9D7F7)
     ) {
     }
-    Column() {
+    Column(
+        modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val total = "%.2f".format(totalPerPerson)
+        Text(text = "Total Per Person", style = MaterialTheme.typography.headlineSmall)
+        Text(
+            text = "$$total",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.displaySmall,
+            fontWeight = FontWeight.Bold
+        )
 
     }
 }
