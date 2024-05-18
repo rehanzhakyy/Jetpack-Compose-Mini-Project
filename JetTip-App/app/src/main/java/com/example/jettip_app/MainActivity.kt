601126dedb870fa1e9fe771f24a63a70681bce2b
rehanzhakyy <hannn9491@gmail.com>
2024-05-18 19:25:00 +0700

    the structure of jettip app

--- JetTip-App/app/src/main/java/com/example/jettip_app/MainActivity.kt
+++ JetTip-App/app/src/main/java/com/example/jettip_app/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jettip_app.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +28,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetTipAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                Text(text = "Hello world")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyApp(content: @Composable () -> Unit) {
+
     JetTipAppTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
     }
 }
+
+@Composable
+@Preview
+fun TopHeader() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(
+                shape = RoundedCornerShape(corner = CornerSize(12.dp))
+            )
+    ) {
+    }
+    Column() {
+
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    JetTipAppTheme {
+//        MyApp {
+//            Text(text = "Hello world")
+//        }
+//    }
+//}
